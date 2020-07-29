5b6fcddf37ba0a8a1905ec3150ec1b58632d68cd
pixelresponse <naumovskig@yahoo.com>
2020-07-29 23:08:20 +0100

    init commit

---
+++ client/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+  return <h1>Hello Wepack 33!</h1>;
+};
+ReactDOM.render(<App />, document.getElementById('root'));
